0c126e2366efc18f28fc4906deeafe3b647e4247
Edmundo Corona <113280885+edmundoCA@users.noreply.github.com>
2024-04-07 14:01:22 +0000

    added state

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const generateBtn = document.getElementById('generate');
 const label = document.getElementById('numberRandom');
+const stateLbl = document.getElementById('state');
 
 generateBtn.addEventListener('click', () => {
-    var number = Math.random();
-    label.textContent = numberClosure(
+    var number  = numberClosure(
         getRandomIntInclusive(2, 9),
         getRandomIntInclusive(1, 9),
         getRandomIntInclusive(0, 9),
@@ -14,7 +14,42 @@ generateBtn.addEventListener('click', () => {
         getRandomIntInclusive(0, 9),
         getRandomIntInclusive(0, 9),
         getRandomIntInclusive(0, 9));
+    label.textContent = number;
+    stateLbl.textContent = getState(+ number.charAt(0));
 });
+
+function getState(number) {
+    var estados;
+    switch (number) {
+        case 2:
+            estados = ["Puebla", "Veracruz", "Tlaxcala", "Hidalgo"];
+            break;
+        case 3:
+            estados = ["Jalisco", "Colima", "Nayarit"];
+            break;
+        case 6:
+            estados = ["Sinaloa", "Durango", "Chihuahua", "Sonora", "Baja California", "Baja California Sur"];
+            break;
+        case 5:
+            estados = ["CDMX", "Edo. de México"];
+            break;
+        case 4:
+            estados = ["Zacatecas", "San Luis Potosí", "Aguascalientes", "Guanajuato", "Querétaro", "Michoacán"];
+            break;
+        case 7:
+            estados = ["Guerrero", "Morelos"];
+            break;
+        case 8:
+            estados = ["Coahuila", "Nuevo León", "Tamaulipas", ];
+            break;
+        case 9:
+            estados = ["Oaxaca", "Chiapas", "Tabasco", "Campeche", "Yucatán", "Quintana Roo"];
+            break;
+        default:
+            throw new Error("Invalid number");
+    }
+    return estados[getRandomIntInclusive(0, estados.length - 1)];
+}
 
 function getRandomIntInclusive(min, max) {
     const minCeiled = Math.ceil(min);
